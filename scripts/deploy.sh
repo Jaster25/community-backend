78bfbb267021236185a93c2e2c2d422ed509af0c
Jaster25 <js_328@naver.com>
2022-07-14 14:50:01 +0900

    [#12] Setup: 자동 배포 환경 설정

---
+++ scripts/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+REPOSITORY=/home/ubuntu/action
+PROJECT_NAME=community-backend
+
+echo "> Build 파일 복사"
+cp $REPOSITORY/build/libs/*.jar $REPOSITORY/
+
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep java | awk '{print $1}')
+
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  ehco "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 10
+fi
+
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR name: JAR_NAME"
+echo "> $JAR_NAME에 실행 권한 추가"
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar -Dspring.profiles.active=prod JASYPT_PASSWORD=$JASYPT_PASSWORD $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
